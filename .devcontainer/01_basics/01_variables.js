756fef472b83c13ec5366c1579421a0cd62e46b1
Dawa Theeng <137314420+DawaTheeng@users.noreply.github.com>
2024-09-12 07:30:20 +0000

    prettier

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12233
+let accountEmail = "Dawa@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kathmandu"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "rahul@gmail.com"
+accountPassword = "1444333"
+accountCity = "Pokhara"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all JS code as never version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log(Dawa)
+
