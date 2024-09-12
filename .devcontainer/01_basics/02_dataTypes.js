61e50ebdca1ffc7f0f0e99ee1edb82de1c6c4d5b
Dawa Theeng <137314420+DawaTheeng@users.noreply.github.com>
2024-09-12 08:00:20 +0000

    datatypes

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -2,7 +2,27 @@
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 + 3)
+console.log(3
+     + 
+     3) // code readability should be high
 
-console.log(Dawa)
+// console.log("Dawaa")
+
+let name = "Dawa"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+// object
+
+console.log(typeof "undefined"); // undefined
+console.log(typeof null); // object
+
+
 
