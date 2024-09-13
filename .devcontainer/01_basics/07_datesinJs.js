7216578e1b2384db07c4636ef34da9c59f9dceb7
Dawa Theeng <137314420+DawaTheeng@users.noreply.github.com>
2024-09-13 16:22:06 +0000

    datesInJs

---
+++ .devcontainer/01_basics/07_datesinJs.js
@@ -0,0 +1,13 @@
+// Dates 
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024, 10, 13)
+console.log(myCreatedDate.toDateString());
